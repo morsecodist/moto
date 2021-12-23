d9c0a17f20cf5bc6989d1a5cb44733b96bae17ad
morsecodist <todd@morsecodist.io>
2021-12-23 12:37:09 -0500

    host-experiment

--- moto/awslambda/models.py
+++ moto/awslambda/models.py
@@ -583,7 +583,8 @@ class LambdaFunction(CloudFormationModel, DockerModel):
                     #   for communication with other mock AWS services running on localhost
                     if platform == "linux" or platform == "linux2":
                         run_kwargs["extra_hosts"] = {
-                            "host.docker.internal": "host-gateway"
+                            "host.docker.internal": "host-gateway",
+                            "host.docker.internal": "test-bucket.host.docker.internal",
                         }
 
                     image_ref = "lambci/lambda:{}".format(self.run_time)
